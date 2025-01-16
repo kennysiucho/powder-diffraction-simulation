b595aa3b799d0b6545f144761ff43c6e107ed16a
Joe Endacott <joseph.endacott@gmail.com>
2025-01-16 20:31:12 +0000

    Updated main.py.

--- main.py
+++ main.py
@@ -1,28 +1,13 @@
-import B8_project.extract_parameters as extract_parameters
+from B8_project import extract_parameters
 import B8_project.unit_cell as unit_cell
 
 LATTICE_FILE = "Parameters/lattice.csv"
 BASIS_FILE = "Parameters/basis.csv"
 
-material, lattice_type, lattice_constants = extract_parameters.get_lattice_from_csv(
-    LATTICE_FILE
-)
+lattice = extract_parameters.get_lattice_from_csv(LATTICE_FILE)
 
-atomic_numbers, atomic_masses, atomic_positions = extract_parameters.get_basis_from_csv(
-    BASIS_FILE
-)
+basis = extract_parameters.get_basis_from_csv(BASIS_FILE)
 
-print(
-    f"material = {material}, lattice_type = {lattice_type}, lattice_constants = {lattice_constants}"
-)
-
-print(
-    f"atomic numbers = {atomic_numbers}, atomic masses = {atomic_masses}, "
-    f"atomic positions = {atomic_positions}"
-)
-
-lattice = material, lattice_type, lattice_constants
-basis = atomic_numbers, atomic_masses, atomic_positions
 try:
     my_cell = unit_cell.UnitCell.parameters_to_unit_cell(lattice, basis)
     print(f"{my_cell}")
