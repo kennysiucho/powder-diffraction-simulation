c83132a3a1a1d88a8dcc1f03d56b51f4f93d025a
Joe Endacott <joseph.endacott@gmail.com>
2025-01-19 18:29:51 +0000

    Added  dot product function to utils.py. Moved ReciprocalLatticeVector class to a new file, reciprocal_lattice_vectors.py. Added a new file diffraction_pattern.py and wrote a function which calculates the structure factor for neutron diffraction.

---
+++ B8_project/diffraction_pattern.py
@@ -0,0 +1,45 @@
+"""
+TODO: add unit tests for functions in this module.
+"""
+
+import math
+import cmath
+import utils
+from B8_project.unit_cell import UnitCell
+from B8_project.reciprocal_lattice_vectors import ReciprocalLatticeVector
+
+
+def get_neutron_structure_factor(
+    unit_cell: UnitCell,
+    neutron_scattering_length: dict[int, float],
+    reciprocal_lattice_vector: ReciprocalLatticeVector,
+):
+    """
+    Get neutron structure factor
+    ============================
+
+    Returns the structure factor of a crystal evaluated at a given reciprocal lattice
+    vector.
+
+    An instance of `UnitCell` represents the crystal. The neutron scattering lengths
+    are stored in a dictionary which maps atomic number to neutron scattering length.
+
+    Parameters
+    ----------
+    TODO: add parameters.
+
+    Returns
+    -------
+    TODO: add returns.
+    """
+    structure_factor = 0
+    for atom in unit_cell.atoms:
+        exponent = (2 * math.pi * 1j) * utils.dot_product_tuples(
+            reciprocal_lattice_vector.miller_indices, atom.position
+        )
+
+        structure_factor += neutron_scattering_length[atom.atomic_number] * cmath.exp(
+            exponent
+        )
+
+    return structure_factor
--- B8_project/utils.py
+++ B8_project/utils.py
@@ -42,3 +42,28 @@ def add_tuples(
         the corresponding elements of `tuple_1` and `tuple_2`.
     """
     return (tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1], tuple_1[2] + tuple_2[2])
+
+
+def dot_product_tuples(
+    tuple_1: tuple[float, float, float], tuple_2: tuple[float, float, float]
+) -> float:
+    """
+    Dot product tuples
+    ==========
+
+    Computes the dot product of two tuples. Both tuples should have three elements.
+
+    Parameters
+    ----------
+        - tuple_1 (tuple[float, float, float]): The first tuple.
+        - tuple_2 (tuple[float, float, float]): The second tuple.
+
+    Returns
+    -------
+        - float: The dot product of tuple_1 and tuple_2.
+    """
+    return (
+        (tuple_1[0] * tuple_2[0])
+        + (tuple_1[1] * tuple_2[1])
+        + (tuple_1[2] * tuple_2[2])
+    )
