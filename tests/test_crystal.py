ef6100d5cf997c5ac981077a2b611a1271424410
Joe Endacott <joseph.endacott@gmail.com>
2025-01-22 15:46:07 +0000

    Added tests for XRayFormFactor class.

--- tests/test_crystal.py
+++ tests/test_crystal.py
@@ -14,6 +14,7 @@ from B8_project.crystal import (
     UnitCell,
     ReciprocalLatticeVector,
     NeutronFormFactor,
+    XRayFormFactor,
 )
 from B8_project import file_reading
 from B8_project import utils
@@ -304,6 +305,52 @@ class TestXRayFormFactor:
     """
     Unit tests for the `XRayFormFactor` class.
     """
+
+    @staticmethod
+    def test_xray_form_factor_initialization_normal_operation():
+        """
+        A unit test that tests the initialization of a `XRayFormFactor` instance.
+        This unit test tests initialization with normal attributes.
+        """
+        xray_form_factor = XRayFormFactor(1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+        assert xray_form_factor.a1 == 1
+        assert xray_form_factor.b1 == 2
+        assert xray_form_factor.a2 == 3
+        assert xray_form_factor.b2 == 4
+        assert xray_form_factor.a3 == 5
+        assert xray_form_factor.b3 == 6
+        assert xray_form_factor.a4 == 7
+        assert xray_form_factor.b4 == 8
+        assert xray_form_factor.c == 9
+
+    @staticmethod
+    def test_get_form_factor():
+        """
+        A unit test for the get_form_factor function. This unit test tests normal
+        operation of the function.
+        """
+        x_ray_form_factor = XRayFormFactor(1, 2, 3, 4, 5, 6, 7, 8, 9)
+        reciprocal_lattice_vector = ReciprocalLatticeVector((1, 0, 0), (1, 1, 1))
+
+        assert math.isclose(
+            reciprocal_lattice_vector.get_magnitude(), 2 * math.pi, rel_tol=1e-6
+        )
+
+        result = x_ray_form_factor.get_form_factor(reciprocal_lattice_vector)
+
+        a = [1, 3, 5, 7]
+        b = [2, 4, 6, 8]
+        c = 9
+
+        expected_result = 0
+
+        for i in range(4):
+            expected_result += a[i] * math.exp(-b[i] / 4)
+
+        expected_result += c
+
+        assert math.isclose(result, expected_result, rel_tol=1e-6)
 
 
 class TestDiffraction:
--- tests/test_diffraction_pattern.py
+++
@@ -1,4 +0,0 @@
-"""
-This module contains unit tests for the diffraction_pattern.py module.
-TODO: add tests for functions in diffraction_pattern.py.
-"""
