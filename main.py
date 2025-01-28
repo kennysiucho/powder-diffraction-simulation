c3a449a5c9d771703296baf80e7e84811378fab4
Joe Endacott <joseph.endacott@gmail.com>
2025-01-28 09:42:09 +0000

    Added templates for crystal parameters for a range of type III-V semiconductors. Plotted diffraction spectra for AlX semiconductors, GaAs and InAs.

--- main.py
+++ main.py
@@ -7,23 +7,51 @@ from B8_project import file_reading
 from B8_project import crystal, diffraction
 
 # Get basis, lattice and neutron form factors from CSV files.
-basis = file_reading.read_basis("data/PrO2_basis.csv")
-lattice = file_reading.read_lattice("data/PrO2_lattice.csv")
+GaAs_basis = file_reading.read_basis("data/GaAs_basis.csv")
+GaAs_lattice = file_reading.read_lattice("data/GaAs_lattice.csv")
+InAs_basis = file_reading.read_basis("data/InAs_basis.csv")
+InAs_lattice = file_reading.read_lattice("data/InAs_lattice.csv")
+AlAs_basis = file_reading.read_basis("data/AlAs_basis.csv")
+AlAs_lattice = file_reading.read_lattice("data/AlAs_lattice.csv")
+AlP_basis = file_reading.read_basis("data/AlP_basis.csv")
+AlP_lattice = file_reading.read_lattice("data/AlP_lattice.csv")
+AlSb_basis = file_reading.read_basis("data/AlSb_basis.csv")
+AlSb_lattice = file_reading.read_lattice("data/AlSb_lattice.csv")
+
 neutron_form_factors = file_reading.read_neutron_scattering_lengths(
     "data/neutron_scattering_lengths.csv"
 )
+x_ray_form_factors = file_reading.read_xray_form_factors("data/x_ray_form_factors.csv")
 
-# Calculate the unit cell
-unit_cell = crystal.UnitCell.new_unit_cell(basis, lattice)
+# Calculate the unit cells.
+GaAs_unit_cell = crystal.UnitCell.new_unit_cell(GaAs_basis, GaAs_lattice)
+InAs_unit_cell = crystal.UnitCell.new_unit_cell(InAs_basis, InAs_lattice)
+AlAs_unit_cell = crystal.UnitCell.new_unit_cell(AlAs_basis, AlAs_lattice)
+AlP_unit_cell = crystal.UnitCell.new_unit_cell(AlP_basis, AlP_lattice)
+AlSb_unit_cell = crystal.UnitCell.new_unit_cell(AlSb_basis, AlSb_lattice)
 
-# Plot the diffraction pattern
-print(
-    diffraction.plot_diffraction_pattern(
-        unit_cell,
-        neutron_form_factors,
-        0.123,
-        20,
-        55,
-        0.05,
+# Make a list of the unit cells.
+unit_cells = [
+    GaAs_unit_cell,
+    InAs_unit_cell,
+    AlAs_unit_cell,
+    AlP_unit_cell,
+    AlSb_unit_cell,
+]
+
+# Plot the XRD and ND patterns.
+for unit_cell in unit_cells:
+    print(
+        diffraction.plot_diffraction_pattern(
+            unit_cell,
+            neutron_form_factors,
+            0.1,
+            10,
+            90,
+            0.1,
+        )
+        + "\n"
+        + diffraction.plot_diffraction_pattern(
+            unit_cell, x_ray_form_factors, 0.1, 10, 90, 0.1
+        )
     )
-)
