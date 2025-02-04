0744e5102da91a50051a97f123871ee014e95273
Joe Endacott <joseph.endacott@gmail.com>
2025-02-04 08:15:54 +0000

    Created a benchmark module, and added a function which benchmarks a specified function.

---
+++ benchmarks/benchmark.py
@@ -0,0 +1,32 @@
+"""
+This module contains functions used to benchmark the code in this project.
+"""
+
+import statistics
+import time
+
+
+def benchmark_function(function, *args, number_of_runs):
+    """
+    Benchmark function
+    ==================
+
+    Calls a specified function with specified arguments, and times how long it takes
+    for the function to finish execution. Repeats this process a specified number of
+    times, and returns the output from the function, the average execution time and the
+    standard deviation in the execution times.
+    """
+    times = []
+
+    for _ in range(number_of_runs):
+        # Start the timer.
+        start_time = time.perf_counter()
+
+        # Execute the function.
+        result = function(*args)
+
+        # Stop the timer.
+        stop_time = time.perf_counter()
+        times.append(stop_time - start_time)
+
+    return result, statistics.mean(times), statistics.stdev(times)
