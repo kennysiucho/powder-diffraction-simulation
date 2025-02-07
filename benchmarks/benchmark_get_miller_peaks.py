c3733f5c61039dd762638940d2d98041fa7ba798
Joe Endacott <joseph.endacott@gmail.com>
2025-02-07 07:33:33 +0000

    Removed benchmarks for old code. Updated benchmarks for new code.

--- benchmarks/benchmark_get_miller_peaks.py
+++ benchmarks/benchmark_get_miller_peaks.py
@@ -15,7 +15,7 @@ from B8_project import (
 # Get a GaAs unit cell.
 basis = file_reading.read_basis("data/GaAs_basis.csv")
 lattice = file_reading.read_lattice("data/GaAs_lattice.csv")
-GaAs_unit_cell = crystal.UnitCellV2.new_unit_cell(basis, lattice)
+GaAs_unit_cell = crystal.UnitCell.new_unit_cell(basis, lattice)
 
 # Read X-ray form factors.
 x_ray_form_factors = file_reading.read_xray_form_factors("data/x_ray_form_factors.csv")
@@ -27,9 +27,9 @@ neutron_form_factors = file_reading.read_neutron_scattering_lengths(
 # Generate GaAs super cells.
 GaAs_super_cells = []
 
-MAX_SIDE_LENGTH = 5
+MAX_SIDE_LENGTH = 3
 for side_length in range(1, MAX_SIDE_LENGTH + 1):
-    GaAs_super_cell = alloy.SuperCellV2.new_super_cell(
+    GaAs_super_cell = alloy.SuperCell.new_super_cell(
         GaAs_unit_cell, (side_length, side_length, side_length), "GaAs"
     ).to_unit_cell()
 
@@ -53,6 +53,7 @@ for i, GaAs_super_cell in enumerate(GaAs_super_cells):
         intensity_cutoff=1e-6,
         print_peak_data=False,
         save_to_csv=False,
+        number_of_runs=3,
     )
     benchmark_data.append((i + 1, average_time, std_dev_time))
 
