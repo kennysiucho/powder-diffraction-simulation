b5923cd5b6450cceab01c6e6860c7f229e230dda
Joe Endacott <joseph.endacott@gmail.com>
2025-02-10 14:06:36 +0000

    Updated the tests in the test_alloy.py module.

--- tests/test_alloy.py
+++ tests/test_alloy.py
@@ -13,51 +13,16 @@ class TestSuperCell:
     """
 
     @staticmethod
-    def test_super_cell_initialization_normal_operation():
-        """
-        A unit test that tests the initialization of a `SuperCell` instance. This unit
-        test tests initialization with normal attributes.
-        """
-        basis = file_reading.read_basis("tests/data/CsCl_basis.csv")
-        lattice = file_reading.read_lattice("tests/data/CsCl_lattice.csv")
-        unit_cell = crystal.UnitCell.new_unit_cell(basis, lattice)
-
-        side_lengths = np.array([2, 2, 2])
-        super_cell = alloy.SuperCell("CsCl", unit_cell, side_lengths)
-
-        assert super_cell.unit_cell == unit_cell
-        assert np.array_equal(super_cell.side_lengths, side_lengths)
-
-    @staticmethod
-    def test_new_super_cell_normal_operation():
-        """
-        A unit test that tests the new_super_cell method of the SuperCell class. This
-        unit test tests the normal operation of the new_super_cell method.
-        """
-        basis = file_reading.read_basis("tests/data/CsCl_basis.csv")
-        lattice = file_reading.read_lattice("tests/data/CsCl_lattice.csv")
-        unit_cell = crystal.UnitCell.new_unit_cell(basis, lattice)
-
-        super_cell = alloy.SuperCell.new_super_cell(unit_cell, (2, 2, 2))
-
-        assert super_cell.unit_cell == unit_cell
-        assert np.array_equal(super_cell.side_lengths, np.array([2, 2, 2]))
-
-    @staticmethod
     def test_get_lattice_vectors_normal_operation():
         """
         A unit test that tests the get_lattice_vectors method of the SuperCell class.
         This unit test tests the normal operation of the get_lattice_vectors method.
         """
-        basis = file_reading.read_basis("tests/data/CsCl_basis.csv")
-        lattice = file_reading.read_lattice("tests/data/CsCl_lattice.csv")
-        unit_cell = crystal.UnitCell.new_unit_cell(basis, lattice)
+        # pylint: disable=protected-access
+        lattice_vectors = alloy.SuperCell._get_lattice_vectors((1, 1, 1))
+        assert np.array_equal(lattice_vectors, np.array([[0, 0, 0]]))
 
-        super_cell = alloy.SuperCell.new_super_cell(unit_cell, (1, 1, 1))
-        assert super_cell.lattice_vectors() is not None
-        assert np.array_equal(super_cell.lattice_vectors(), np.array([[0, 0, 0]]))
-
-        super_cell = alloy.SuperCell.new_super_cell(unit_cell, (2, 2, 2))
+        lattice_vectors = alloy.SuperCell._get_lattice_vectors((2, 2, 2))
         expected_lattice_vectors = np.array(
             [
                 (0, 0, 0),
@@ -72,15 +37,16 @@ class TestSuperCell:
         )
 
         assert np.array_equal(
-            super_cell.lattice_vectors(),
+            lattice_vectors,
             expected_lattice_vectors[np.lexsort(expected_lattice_vectors.T)],
         )
+        # pylint: enable=protected-access
 
     @staticmethod
-    def test_to_unit_cell_normal_operation():
+    def test_new_super_cell_normal_operation():
         """
-        A unit test that tests the to_unit_cell method of the SuperCell class. This
-        unit test tests the normal operation of the to_unit_cell method.
+        A unit test that tests the new_super_cell method of the SuperCell class. This
+        unit test tests the normal operation of the new_super_cell method.
         """
         # 1*1*1 CsCl super cell.
         basis = file_reading.read_basis("tests/data/CsCl_basis.csv")
@@ -88,13 +54,10 @@ class TestSuperCell:
         unit_cell = crystal.UnitCell.new_unit_cell(basis, lattice)
 
         super_cell = alloy.SuperCell.new_super_cell(unit_cell, (1, 1, 1))
-        new_unit_cell = super_cell.to_unit_cell()
 
-        assert unit_cell.material == new_unit_cell.material
-        assert np.array_equal(
-            unit_cell.lattice_constants, new_unit_cell.lattice_constants
-        )
-        assert np.array_equal(unit_cell.atoms, new_unit_cell.atoms)
+        assert super_cell.material == unit_cell.material
+        assert np.array_equal(unit_cell.lattice_constants, super_cell.lattice_constants)
+        assert np.array_equal(unit_cell.atoms, super_cell.atoms)
 
         # 2*2*2 Na super cell.
         basis = file_reading.read_basis("tests/data/Na_basis.csv")
@@ -102,11 +65,10 @@ class TestSuperCell:
         unit_cell = crystal.UnitCell.new_unit_cell(basis, lattice)
 
         super_cell = alloy.SuperCell.new_super_cell(unit_cell, (2, 2, 2))
-        new_unit_cell = super_cell.to_unit_cell()
 
-        assert unit_cell.material == new_unit_cell.material
+        assert unit_cell.material == super_cell.material
         assert np.array_equal(
-            2 * unit_cell.lattice_constants, new_unit_cell.lattice_constants
+            2 * unit_cell.lattice_constants, super_cell.lattice_constants
         )
 
         expected_atomic_positions = np.array(
@@ -131,8 +93,6 @@ class TestSuperCell:
         )
 
         assert np.array_equal(
-            new_unit_cell.atoms["positions"][
-                np.lexsort(new_unit_cell.atoms["positions"].T)
-            ],
+            super_cell.atoms["positions"][np.lexsort(super_cell.atoms["positions"].T)],
             expected_atomic_positions[np.lexsort(expected_atomic_positions.T)],
         )
