f25265b2e3b84635073f15482bb44e35263745e1
Joe Endacott <joseph.endacott@gmail.com>
2025-02-10 08:31:22 +0000

    Updated tests in the file_reading.py module. Removed the redundant test_super_cell.py module.

--- tests/test_file_reading.py
+++ tests/test_file_reading.py
@@ -4,14 +4,7 @@ This module contains unit tests for the file_reading.py module.
 
 from typing import Mapping
 
-from B8_project.file_reading import (
-    read_lattice,
-    read_basis,
-    read_neutron_scattering_lengths,
-    read_xray_form_factors,
-)
-
-from B8_project.form_factor import NeutronFormFactor, XRayFormFactor
+from B8_project import file_reading, form_factor
 
 
 def test_read_lattice_normal_operation():
@@ -20,25 +13,25 @@ def test_read_lattice_normal_operation():
     the function.
     """
     CsCl_lattice = "tests/data/CsCl_lattice.csv"  # pylint: disable=C0103
-    material, lattice_type, lattice_constants = read_lattice(CsCl_lattice)
+    material, lattice_type, lattice_constants = file_reading.read_lattice(CsCl_lattice)
     assert material == "CsCl"
     assert lattice_type == 1
     assert lattice_constants == (0.4119, 0.4119, 0.4119)
 
     Cu_lattice = "tests/data/Cu_lattice.csv"  # pylint: disable=C0103
-    material, lattice_type, lattice_constants = read_lattice(Cu_lattice)
+    material, lattice_type, lattice_constants = file_reading.read_lattice(Cu_lattice)
     assert material == "Cu"
     assert lattice_type == 3
     assert lattice_constants == (0.3615, 0.3615, 0.3615)
 
     Na_lattice = "tests/data/Na_lattice.csv"  # pylint: disable=C0103
-    material, lattice_type, lattice_constants = read_lattice(Na_lattice)
+    material, lattice_type, lattice_constants = file_reading.read_lattice(Na_lattice)
     assert material == "Na"
     assert lattice_type == 2
     assert lattice_constants == (0.4287, 0.4287, 0.4287)
 
     NaCl_lattice = "tests/data/NaCl_lattice.csv"  # pylint: disable=C0103
-    material, lattice_type, lattice_constants = read_lattice(NaCl_lattice)
+    material, lattice_type, lattice_constants = file_reading.read_lattice(NaCl_lattice)
     assert material == "NaCl"
     assert lattice_type == 3
     assert lattice_constants == (0.5640, 0.5640, 0.5640)
@@ -50,22 +43,22 @@ def test_read_basis_normal_operation():
     the function.
     """
     CsCl_basis = "tests/data/CsCl_basis.csv"  # pylint: disable=C0103
-    atomic_numbers, atomic_positions = read_basis(CsCl_basis)
+    atomic_numbers, atomic_positions = file_reading.read_basis(CsCl_basis)
     assert atomic_numbers == [55, 17]
     assert atomic_positions == [(0, 0, 0), (0.5, 0.5, 0.5)]
 
     Cu_basis = "tests/data/Cu_basis.csv"  # pylint: disable=C0103
-    atomic_numbers, atomic_positions = read_basis(Cu_basis)
+    atomic_numbers, atomic_positions = file_reading.read_basis(Cu_basis)
     assert atomic_numbers == [29]
     assert atomic_positions == [(0, 0, 0)]
 
     Na_basis = "tests/data/Na_basis.csv"  # pylint: disable=C0103
-    atomic_numbers, atomic_positions = read_basis(Na_basis)
+    atomic_numbers, atomic_positions = file_reading.read_basis(Na_basis)
     assert atomic_numbers == [11]
     assert atomic_positions == [(0, 0, 0)]
 
     NaCl_basis = "tests/data/NaCl_basis.csv"  # pylint: disable=C0103
-    atomic_numbers, atomic_positions = read_basis(NaCl_basis)
+    atomic_numbers, atomic_positions = file_reading.read_basis(NaCl_basis)
     assert atomic_numbers == [11, 17]
     assert atomic_positions == [(0, 0, 0), (0.5, 0.5, 0.5)]
 
@@ -77,15 +70,16 @@ def test_read_neutron_scattering_lengths_normal_operation():
     """
     filename = "tests/data/neutron_scattering_lengths.csv"
 
-    neutron_scattering_lengths = read_neutron_scattering_lengths(filename)
+    neutron_scattering_lengths = file_reading.read_neutron_scattering_lengths(filename)
 
-    assert neutron_scattering_lengths[11] == NeutronFormFactor(3.63)
-    assert neutron_scattering_lengths[17] == NeutronFormFactor(9.5792)
-    assert neutron_scattering_lengths[29] == NeutronFormFactor(7.718)
-    assert neutron_scattering_lengths[55] == NeutronFormFactor(5.42)
+    assert neutron_scattering_lengths[11] == form_factor.NeutronFormFactor(3.63)
+    assert neutron_scattering_lengths[17] == form_factor.NeutronFormFactor(9.5792)
+    assert neutron_scattering_lengths[29] == form_factor.NeutronFormFactor(7.718)
+    assert neutron_scattering_lengths[55] == form_factor.NeutronFormFactor(5.42)
 
     assert isinstance(neutron_scattering_lengths, Mapping) and all(
-        isinstance(v, NeutronFormFactor) for v in neutron_scattering_lengths.values()
+        isinstance(v, form_factor.NeutronFormFactor)
+        for v in neutron_scattering_lengths.values()
     )
 
 
@@ -96,15 +90,15 @@ def test_read_x_ray_form_factors_normal_operation():
     """
     filename = "tests/data/xray_form_factors.csv"
 
-    x_ray_form_factors = read_xray_form_factors(filename)
+    x_ray_form_factors = file_reading.read_xray_form_factors(filename)
 
-    assert x_ray_form_factors[11] == XRayFormFactor(
+    assert x_ray_form_factors[11] == form_factor.XRayFormFactor(
         4.7626, 3.285, 3.1736, 8.8422, 1.2674, 0.3136, 1.1128, 129.424, 0.676
     )
-    assert x_ray_form_factors[17] == XRayFormFactor(
+    assert x_ray_form_factors[17] == form_factor.XRayFormFactor(
         11.4604, 0.0104, 7.1964, 1.1662, 6.2556, 18.5194, 1.6455, 47.7784, -9.5574
     )
 
     assert isinstance(x_ray_form_factors, Mapping) and all(
-        isinstance(v, XRayFormFactor) for v in x_ray_form_factors.values()
+        isinstance(v, form_factor.XRayFormFactor) for v in x_ray_form_factors.values()
     )
