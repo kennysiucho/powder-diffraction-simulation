e9943f5a50e68acb5e516b18b5616582d6dff35c
Joe Endacott <joseph.endacott@gmail.com>
2025-02-10 14:06:23 +0000

    Updated the SuperCell class in the alloy.py module. The class now groups functions related to generating super cells, and has no attributes.

--- B8_project/alloy.py
+++ B8_project/alloy.py
@@ -17,42 +17,45 @@ class SuperCell:
     Super cell
     ==========
 
-    A class to represent a super cell. A super cell is a conventional unit cell made
-    from a collection of smaller, identical unit cells. This class can only represent
-    super cells which are cuboids.
-
-    Attributes
-    ----------
-    material : str
-        The name of the crystal.
-    unit_cell : UnitCell
-        The unit cell which is to be copied to produce the super cell.
-    side_lengths : np.ndarray
-        The side lengths of the super cell, specified in terms of the lattice constants
-        of the unit cell.
+    A class that groups functions related to generating super cells from unit cells.
 
     Methods
     -------
+    _get_lattice_vectors
+        Returns the position vector of each unit cell in the super cell.
     new_super_cell
         Generates a new super cell.
-    lattice_vectors
-        Returns the position vector of each unit cell in the super cell.
-    to_unit_cell
-        Converts a super cell into an instance of `UnitCell`, i.e. a list of atoms and
-        atomic positions inside the super cell.
     """
 
-    material: str
-    unit_cell: UnitCell
-    side_lengths: np.ndarray
+    @staticmethod
+    def _get_lattice_vectors(side_lengths: tuple[int, int, int]):
+        """
+        Get lattice vectors
+        ===================
 
-    def __post_init__(self):
-        if not isinstance(self.side_lengths, np.ndarray):
-            raise TypeError("side_lengths must be a numpy array.")
-        if self.side_lengths.dtype != int:
-            raise TypeError("side_lengths must be an array of integers.")
-        if self.side_lengths.shape != (3,):
-            raise ValueError("side_lengths must be of length 3.")
+        Returns the position vector of each unit cell in the super cell, in terms of
+        the lattice constants.
+        """
+        x_length, y_length, z_length = side_lengths
+
+        # Error handling for if any of the lengths are 0 or negative.
+        if x_length <= 0 or y_length <= 0 or z_length <= 0:
+            raise ValueError("All entries in side_lengths must be greater than 0.")
+
+        # lattice_vectors stores the position vectors of the corners of the unit cells
+        # comprising the super cell.
+        lattice_vectors = np.array(
+            [
+                [x, y, z]
+                for x in range(x_length)
+                for y in range(y_length)
+                for z in range(z_length)
+            ]
+        )
+
+        sorted_lattice_vectors = lattice_vectors[np.lexsort(lattice_vectors.T)]
+
+        return sorted_lattice_vectors
 
     @classmethod
     def new_super_cell(
@@ -88,49 +91,9 @@ class SuperCell:
         if side_lengths[0] <= 0 or side_lengths[1] <= 0 or side_lengths[2] <= 0:
             raise ValueError("All entries in side_lengths must be greater than 0.")
 
-        return cls(material, unit_cell, np.array(side_lengths))
+        lattice_vectors = SuperCell._get_lattice_vectors(side_lengths)
 
-    def lattice_vectors(self):
-        """
-        Get lattice vectors
-        ===================
-
-        Returns the position vector of each unit cell in the super cell, in terms of
-        the lattice constants.
-        """
-        x_length, y_length, z_length = self.side_lengths
-
-        # Error handling for if any of the lengths are 0 or negative.
-        if x_length <= 0 or y_length <= 0 or z_length <= 0:
-            raise ValueError("All entries in side_lengths must be greater than 0.")
-
-        # lattice_vectors stores the position vectors of the corners of the unit cells
-        # comprising the super cell.
-        lattice_vectors = np.array(
-            [
-                [x, y, z]
-                for x in range(x_length)
-                for y in range(y_length)
-                for z in range(z_length)
-            ]
-        )
-
-        sorted_lattice_vectors = lattice_vectors[np.lexsort(lattice_vectors.T)]
-
-        return sorted_lattice_vectors
-
-    def to_unit_cell(self) -> UnitCell:
-        """
-        Super cell to unit cell
-        =======================
-
-        Converts a super cell into an instance of `UnitCell`, i.e. a list of atoms in
-        the super cell and their positions.
-        """
-        unit_cell = self.unit_cell
-        lattice_vectors = self.lattice_vectors()
-
-        lattice_constants = self.side_lengths * unit_cell.lattice_constants
+        lattice_constants = np.array(side_lengths) * unit_cell.lattice_constants
 
         atomic_numbers = unit_cell.atoms["atomic_numbers"]
         atomic_positions = unit_cell.atoms["positions"]
@@ -140,7 +103,7 @@ class SuperCell:
         atomic_numbers = np.repeat(atomic_numbers, len(lattice_vectors))
         atomic_positions = (
             atomic_positions[:, np.newaxis, :] + lattice_vectors
-        ) / self.side_lengths
+        ) / np.array(side_lengths)
 
         # Define a custom datatype to represent atoms.
         dtype = np.dtype([("atomic_numbers", "i4"), ("positions", "3f8")])
@@ -150,4 +113,15 @@ class SuperCell:
         atoms["atomic_numbers"] = atomic_numbers
         atoms["positions"] = atomic_positions.reshape(-1, 3)
 
-        return UnitCell(self.material, lattice_constants, atoms)
+        return UnitCell(material, lattice_constants, atoms)
+
+
+# class DisorderedAlloy:
+#     """
+#     Disordered alloy
+#     ================
+
+#     This class groups functions related to disordered alloys.
+#     """
+#     @staticmethod
+#     def apply_disorder(super_cell: SuperCell, ):
