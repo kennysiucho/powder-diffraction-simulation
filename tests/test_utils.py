c94a23820e61566c91737ba9526e9cc32598c731
Kenny Cho <kennycho168@outlook.com>
2025-02-13 23:27:15 +0000

    Add flag to skip visual tests, change util import style

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,26 +7,21 @@ import numpy as np
 import numpy.testing as nptest
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from B8_project.utils import (
-    duplicate_elements,
-    add_tuples,
-    dot_product_tuples,
-    gaussian,
-    random_uniform_unit_vector,
-    random_uniform_unit_vectors,
-)
+import pytest
+from B8_project import utils
 
+RUN_VISUAL_TESTS = False
 
 def test_duplicate_elements_normal_operation():
     """
     A unit test for the duplicate_elements function. This unit test tests normal
     operation of the function.
     """
-    assert duplicate_elements([0], 1) == [0]
-    assert duplicate_elements(["a"], 2) == ["a", "a"]
-    assert duplicate_elements([1, 2, 3], 1) == [1, 2, 3]
-    assert duplicate_elements([4, 5, 6], 2) == [4, 4, 5, 5, 6, 6]
-    assert duplicate_elements(["a", "b", "c"], 3) == [
+    assert utils.duplicate_elements([0], 1) == [0]
+    assert utils.duplicate_elements(["a"], 2) == ["a", "a"]
+    assert utils.duplicate_elements([1, 2, 3], 1) == [1, 2, 3]
+    assert utils.duplicate_elements([4, 5, 6], 2) == [4, 4, 5, 5, 6, 6]
+    assert utils.duplicate_elements(["a", "b", "c"], 3) == [
         "a",
         "a",
         "a",
@@ -44,9 +39,9 @@ def test_add_tuples_normal_operation():
     A unit test for the add_tuples function. This unit test tests normal operation of
     the function.
     """
-    assert add_tuples((0, 0, 0), (0, 0, 0)) == (0, 0, 0)
-    assert add_tuples((1, 2, 3), (4, 5, 6)) == (5, 7, 9)
-    assert add_tuples((0.5, 1.0, 1.5), (0.25, 0.5, 0.75)) == (0.75, 1.5, 2.25)
+    assert utils.add_tuples((0, 0, 0), (0, 0, 0)) == (0, 0, 0)
+    assert utils.add_tuples((1, 2, 3), (4, 5, 6)) == (5, 7, 9)
+    assert utils.add_tuples((0.5, 1.0, 1.5), (0.25, 0.5, 0.75)) == (0.75, 1.5, 2.25)
 
 
 def test_dot_product_tuples_normal_operation():
@@ -54,10 +49,10 @@ def test_dot_product_tuples_normal_operation():
     A unit test for the dot_product_tuples function. This unit test tests normal
     operation of the function.
     """
-    assert dot_product_tuples((0, 0, 0), (0, 0, 0)) == 0
-    assert dot_product_tuples((1, 2, 3), (4, 5, 6)) == 32
+    assert utils.dot_product_tuples((0, 0, 0), (0, 0, 0)) == 0
+    assert utils.dot_product_tuples((1, 2, 3), (4, 5, 6)) == 32
     assert np.isclose(
-        dot_product_tuples((0.5, 0.5, 0.5), (0.6, 0.6, 0.6)), 0.9, rtol=1e-6
+        utils.dot_product_tuples((0.5, 0.5, 0.5), (0.6, 0.6, 0.6)), 0.9, rtol=1e-6
     )
 
 
@@ -72,7 +67,7 @@ def test_gaussian_normal_operation():
     amplitude = 2
 
     assert np.isclose(
-        gaussian(x, mean, width, amplitude),
+        utils.gaussian(x, mean, width, amplitude),
         amplitude * np.exp(-0.5 * ((x - mean) / width) ** 2),
         rtol=1e-6,
     )
@@ -85,7 +80,7 @@ def test_random_uniform_unit_vector_is_unit_vector():
 
     n = 500
     for dim in range(2, 5):
-        vectors = [random_uniform_unit_vector(dim) for _ in range(n)]
+        vectors = [utils.random_uniform_unit_vector(dim) for _ in range(n)]
         for vector in vectors:
             mag_squared = 0.0
             for e in vector:
@@ -100,7 +95,7 @@ def test_random_uniform_unit_vectors_are_unit_vectors():
 
     n = 500
     for dim in range(2, 5):
-        vectors = random_uniform_unit_vectors(n, dim)
+        vectors = utils.random_uniform_unit_vectors(n, dim)
         for vector in vectors:
             mag_squared = 0.0
             for e in vector:
@@ -133,8 +128,10 @@ def test_random_uniform_3d_unit_vector():
     """
     Visual test to check if the generated random unit vectors seem spherically uniform
     """
+    if not RUN_VISUAL_TESTS:
+        pytest.skip("Test skipped: visual tests are off.")
     n = 500
-    vectors = [random_uniform_unit_vector(3) for _ in range(n)]
+    vectors = [utils.random_uniform_unit_vector(3) for _ in range(n)]
     vectors = np.array(vectors)
     plot_3d_unit_vectors(
         vectors, "Visual check for spherical uniformity: random_uniform_unit_vector"
@@ -145,8 +142,10 @@ def test_random_uniform_3d_unit_vectors():
     """
     Visual test to check if the generated random unit vectors seem spherically uniform
     """
+    if not RUN_VISUAL_TESTS:
+        pytest.skip("Test skipped: visual tests are off.")
     n = 500
-    vectors = random_uniform_unit_vectors(n, 3)
+    vectors = utils.random_uniform_unit_vectors(n, 3)
     plot_3d_unit_vectors(
         vectors, "Visual check for spherical uniformity: random_uniform_unit_vectors"
     )
