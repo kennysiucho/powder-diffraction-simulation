4fbb37cac2acb18fc6400fc0b4e202a9056d2f5b
Kenny Cho <kennycho168@outlook.com>
2025-02-24 22:26:45 +0000

    Use weighting in script, improve plots

--- scripts/calculate_monte_carlo_nd.py
+++ scripts/calculate_monte_carlo_nd.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from B8_project import file_reading
 import B8_project.crystal as unit_cell
-from B8_project.diffraction_monte_carlo import DiffractionMonteCarlo
+from B8_project.diffraction_monte_carlo import DiffractionMonteCarlo, WeightingFunction
 
 two_thetas_file = Path("two_thetas.txt")
 intensities_file = Path("intensities.txt")
@@ -26,6 +26,11 @@ if two_thetas_file.is_file() or intensities_file.is_file():
 else:
     CALCULATE_SPECTRUM = True
 
+pdf = WeightingFunction.get_gaussians_at_peaks(
+        [21.72, 25.13, 35.84, 42.3, 51.58, 56.6], 0.4, 1)
+# pdf = WeightingFunction.uniform
+# pdf = WeightingFunction.natural_distribution
+
 if CALCULATE_SPECTRUM:
     LATTICE_FILE = "data/GaAs_lattice.csv"
     BASIS_FILE = "data/GaAs_basis.csv"
@@ -37,7 +42,7 @@ if CALCULATE_SPECTRUM:
     diffraction = DiffractionMonteCarlo(unit_cell,
                                         0.123,
                                         min_angle_deg=18,
-                                        max_angle_deg=57)
+                                        max_angle_deg=60)
 
     all_nd_form_factors = file_reading.read_neutron_scattering_lengths(
         "data/neutron_scattering_lengths.csv")
@@ -53,13 +58,16 @@ if CALCULATE_SPECTRUM:
         xrd_form_factors[atom.atomic_number] = all_xray_form_factors[atom.atomic_number]
     xrd_form_factors[49] = all_xray_form_factors[49]
 
+    diffraction.set_pdf(pdf)
+
     two_thetas, intensities = (
         diffraction.calculate_diffraction_pattern_ideal_crystal(
             xrd_form_factors,
             target_accepted_trials=10_000_000,
             unit_cell_reps=(10, 10, 10),
             trials_per_batch=1000,
-            angle_bins=60))
+            angle_bins=200,
+            weighted=True))
     np.savetxt('two_thetas.txt', two_thetas)
     np.savetxt('intensities.txt', intensities)
 else:
@@ -71,9 +79,12 @@ else:
 
 # plt.scatter(two_thetas, intensities, s=2, label="Intensity")
 plt.plot(two_thetas, intensities, color='k', label="Intensity")
+plt.plot(two_thetas, pdf(two_thetas) / np.max(pdf(two_thetas)), "--", label="PDF")
+plt.axhline(0, linestyle="--", color="grey")
 plt.xlabel("Scattering angle (2θ) (deg)")
 plt.ylabel("Normalized intensity")
 # plt.title("In_0.25Ga_0.75As Neutron Diffraction Spectrum")
-plt.title("GaAs Neutron Diffraction Spectrum")
+plt.title("GaAs X-ray Diffraction Spectrum - Gaussians")
 plt.legend()
+plt.grid(linestyle=":")
 plt.show()
