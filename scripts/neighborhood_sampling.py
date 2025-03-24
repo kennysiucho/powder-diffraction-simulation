e4c7512444e5144cebb3c2aa85012399f5bee326
Kenny Cho <kennycho168@outlook.com>
2025-03-24 14:00:24 +0800

    Update neighborhood sampling script for CsPbBr3

--- scripts/neighborhood_sampling.py
+++ scripts/neighborhood_sampling.py
@@ -28,44 +28,54 @@ else:
 # pdf = WeightingFunction.natural_distribution
 
 if CALCULATE_SPECTRUM:
-    LATTICE_FILE = "data/PrO2_lattice.csv"
-    BASIS_FILE = "data/PrO2_basis.csv"
+    LATTICE_FILE = "data/CsPbBr3_lattice.csv"
+    BASIS_FILE = "data/CsPbBr3_basis.csv"
 
     lattice = file_reading.read_lattice(LATTICE_FILE)
     basis = file_reading.read_basis(BASIS_FILE)
 
     unit_cell = unit_cell.UnitCell.new_unit_cell(basis, lattice)
+
+    CONC = 0.6
+    lat = 5.94863082 + CONC * (6.27514229 - 5.94863082)
+    unit_cell.lattice_constants = (lat, lat, lat)
+    print("Lattice constants:", unit_cell.lattice_constants)
+
     diff = DiffractionMonteCarlo(unit_cell,
-                                 1.23,
-                                 min_angle_deg=42,
-                                 max_angle_deg=50)
+                                 1.54,
+                                 min_angle_deg=30,
+                                 max_angle_deg=40)
 
-    all_nd_form_factors = file_reading.read_neutron_scattering_lengths(
-        "data/neutron_scattering_lengths.csv")
-    nd_form_factors = {}
+    # all_nd_form_factors = file_reading.read_neutron_scattering_lengths(
+    #     "data/neutron_scattering_lengths.csv")
+    #     # nd_form_factors = {}
+    #     # for atom in diff.unit_cell.atoms:
+    #     #     nd_form_factors[atom.atomic_number] = all_nd_form_factors[atom.atomic_number]
+    # nd_form_factors[49] = all_nd_form_factors[49]
+
+    all_xray_form_factors = file_reading.read_xray_form_factors(
+        "data/x_ray_form_factors.csv")
+    xrd_form_factors = {}
     for atom in diff.unit_cell.atoms:
-        nd_form_factors[atom.atomic_number] = all_nd_form_factors[atom.atomic_number]
-    nd_form_factors[49] = all_nd_form_factors[49]
-
-    # all_xray_form_factors = file_reading.read_xray_form_factors(
-    #     "data/x_ray_form_factors.csv")
-    # xrd_form_factors = {}
-    # for atom in diff.unit_cell.atoms:
-    #     xrd_form_factors[atom.atomic_number] = all_xray_form_factors[atom.atomic_number]
-    # xrd_form_factors[49] = all_xray_form_factors[49]
+        xrd_form_factors[atom.atomic_number] = all_xray_form_factors[atom.atomic_number]
+    xrd_form_factors[53] = all_xray_form_factors[53]
 
     # diffraction.set_pdf(pdf)
 
     start_time = time.time()
 
+    atom_from, atom_to, prob = 35, 53, CONC
+
     two_thetas, intensities, top, counts = (
-        diff.calculate_diffraction_pattern_ideal_crystal(
-            nd_form_factors,
+        diff.calculate_diffraction_pattern_random_occupation(
+            atom_from,
+            atom_to,
+            prob,
+            xrd_form_factors,
             target_accepted_trials=6_000_000,
             unit_cell_reps=(8, 8, 8),
             trials_per_batch=1000,
-            angle_bins=100,
-            weighted=False))
+            angle_bins=100))
     plt.plot(two_thetas, intensities)
     plt.show()
     plt.plot(two_thetas, counts)
@@ -78,15 +88,16 @@ if CALCULATE_SPECTRUM:
     plt.show()
     plt.plot(top[:, 3])
     plt.show()
-    intensities_neigh, counts_neigh = diff.neighborhood_intensity_ideal_crystal(
+    intensities_neigh, counts_neigh = diff.neighborhood_intensity_random_occupation(
+        atom_from,
+        atom_to,
+        prob,
         top[:, 0:3],
         two_thetas,
-        nd_form_factors,
+        xrd_form_factors,
         unit_cell_reps=(20, 20, 20),
-        cnt_per_point=10
+        cnt_per_point=80
     )
-    plt.plot(two_thetas, intensities_neigh)
-    plt.show()
     plt.plot(two_thetas, counts_neigh)
     plt.show()
     # intensities_neigh *= WeightingFunction.natural_distribution(two_thetas) / counts_neigh
@@ -98,7 +109,7 @@ if CALCULATE_SPECTRUM:
     np.savetxt('intensities.txt', intensities_neigh)
 else:
     two_thetas = np.loadtxt("two_thetas.txt")
-    intensities = np.loadtxt("intensities.txt")
+    intensities_neigh = np.loadtxt("intensities.txt")
 
 # two_thetas = two_thetas[::2]
 # intensities = (intensities[::2] + intensities[1::2]) / 2
@@ -108,9 +119,9 @@ plt.plot(two_thetas, intensities_neigh, color='k', label="Intensity")
 # plt.plot(two_thetas, pdf(two_thetas) / np.max(pdf(two_thetas)), "--", label="PDF")
 plt.axhline(0, linestyle="--", color="grey")
 plt.xlabel("Scattering angle (2θ) (deg)")
-plt.ylabel("Normalized intensity")
+plt.ylabel("Intensity")
 # plt.title("In_0.25Ga_0.75As Neutron Diffraction Spectrum")
-plt.title("PrO2 ND Diffraction Spectrum")
+plt.title("CsPbI3 XRD Diffraction Spectrum")
 plt.legend()
 plt.grid(linestyle=":")
 plt.show()
