082d87452c278b0e42184a9e84c1cd1a9edbdaea
Kenny Cho <kennycho168@outlook.com>
2025-04-19 22:21:52 +0100

    Update analytic spectrum script

--- scripts/analytic_spectrum.py
+++ scripts/analytic_spectrum.py
@@ -1,7 +1,7 @@
 from B8_project import file_reading, crystal, diffraction
 
-basis = file_reading.read_basis("data/CsPbBr3_cubic_basis.csv")
-lattice = file_reading.read_lattice("data/CsPbBr3_cubic_lattice.csv")
+basis = file_reading.read_basis("data/GaAs_basis.csv")
+lattice = file_reading.read_lattice("data/GaAs_lattice.csv")
 
 # Get form factors from CSV files.
 neutron_form_factors = file_reading.read_neutron_scattering_lengths(
@@ -9,13 +9,11 @@ neutron_form_factors = file_reading.read_neutron_scattering_lengths(
 )
 x_ray_form_factors = file_reading.read_xray_form_factors("data/x_ray_form_factors.csv")
 
-# Calculate the unit cells.
 unit_cell = crystal.UnitCell.new_unit_cell(basis, lattice)
 
-# Specify the GaAs diffraction parameters.
 WAVELENGTH = 1.54
-MIN_DEFLECTION_ANGLE = 22
-MAX_DEFLECTION_ANGLE = 40
+MIN_DEFLECTION_ANGLE = 20
+MAX_DEFLECTION_ANGLE = 160
 INTENSITY_CUTOFF = 0.001
 
 # Calculate miller peaks for GaAs.
@@ -47,6 +45,6 @@ print("\nDiffraction peaks.")
 for i, peak in enumerate(hkl_peaks):
     print(
         f"Peak {i+1}: "
-        f"(h, k, l) = {peak[1]}; deflection angle = {round(peak[0], 2)}°; "
-        f"relative intensity = {round(peak[2], 4)}"
+        f"(h, k, l) = {peak[1]}; deflection angle = {round(peak[0], 3)}°; "
+        f"relative intensity = {round(peak[2], 5)}"
     )
